2fb4d0e53871bf8409e725c9d2bbeaf85c04719b
Fernando Rodriguez <fernandorodriguezmolina7@gmail.com>
2026-05-23 07:45:22 -0600

    Clase 2

--- src/test/java/com/example/demo/DemoJpaApplicationTests.java
+++ src/test/java/com/example/demo/DemoJpaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.demojpa;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemojpaApplicationTests {
+class DemoJpaApplicationTests {
 
 	@Test
 	void contextLoads() {
